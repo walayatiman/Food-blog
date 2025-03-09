55bb87afbe1075c5e71a5664cde2cac1f5b099e2
Jevan <48089409+walayatiman@users.noreply.github.com>
2025-03-09 12:59:38 +0800

    first update

---
+++ site.config.js
@@ -0,0 +1,39 @@
+export const siteConfig = {
+  title: "Advanced Water Networks",
+  metaTitle: "Water and wastewater services",
+  description:
+    "Advanced Water Infrastructure Networks supply clean water and wastewater services to customers in newly built residential developments.",
+  socialLinks: {
+    twitter: "https://twitter.com/yourhandle",
+    github: "https://github.com/yourrepo",
+  },
+  announcements: "ORDERS $50+ SHIP FREE",
+  navLinks: [
+    {
+      title: "About",
+      url: "#",
+    },
+    {
+      title: "What We do",
+      url: "#",
+      sub_menu: [
+        {
+          title: "Standpipe hire",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Section 8 Notices",
+      url: "#",
+    },
+    {
+      title: "News",
+      url: "#",
+    },
+    {
+      title: "Contact",
+      url: "#",
+    },
+  ],
+};
